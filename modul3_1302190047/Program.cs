cb6582fd192fea635ffc44c42a287f9359569060
idzharism <idzharism@student.telkomuniversity.ac.id>
2022-03-12 14:23:57 +0700

    commit 2 V2

--- modul3_1302190047/Program.cs
+++ modul3_1302190047/Program.cs
@@ -1,42 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 class KodeBuah
 {
     public enum NamaBuah
     {
-        Apel, 
-        Alprikot, 
-        Alpukat, 
-        Pisang, 
-        Paprika, 
+        Apel,
+        Alprikot,
+        Alpukat,
+        Pisang,
+        Paprika,
         Blackberry,
-        Ceri, 
-        Kelapa, 
-        Jagung, 
-        Kurma, 
-        Durian, 
-        Anggur, 
-        Melon, 
+        Ceri,
+        Kelapa,
+        Jagung,
+        Kurma,
+        Durian,
+        Anggur,
+        Melon,
         Semangka
     };
 
-    public static string getKodeBuah(NamaBuah InputBuah)
+    public string GetKodeBuah(NamaBuah inputBuah)
     {
-        string[] arrayKodeBuah = { "A00", 
-                                    "B00", 
-                                    "C00", 
-                                    "D00", 
-                                    "E00",
-                                    "F00", 
-                                    "H00", 
-                                    "I00",
-                                    "J00", 
-                                    "K00",
-                                    "L00", 
-                                    "M00", 
-                                    "N00",
-                                    "O00"};
-        int indexJawaban = (int)InputBuah;
+        string[] arrayKodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "O00" };
+        int indexJawaban = (int)inputBuah;
         return arrayKodeBuah[indexJawaban];
+    }
+    class program
+    {
+        static void Main(string[] args, KodeBuah kodeBuah)
+        {
+
+
+            KodeBuah.NamaBuah inputNamaBuah = KodeBuah.NamaBuah.Durian;
+            Console.WriteLine("Buah dengan Nama:" + inputNamaBuah + "dengan kode nya ialah:" + kodeBuah.GetKodeBuah(inputNamaBuah));
+
+
+
+        }
     }
 }
